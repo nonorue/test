769686c1b4d31ae41590e978cf08e35243eb9d47
nonorue <nicolairue@outlook.com>
2025-08-17 22:57:10 +0200

    Turte program på Spyder

---
+++ førstemann.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import turtle as tu
+import time as ti
+
+tu.shape('turtle')
+tu.speed(1)
+tu.pencolor('yellow')
+tu.pensize(3)
+tu.turtlesize(3,3,3)
+
+ti.sleep(2)
+tu.forward(200)
+ti.sleep(5)
+tu.left(90)
+tu.forward(200)
+tu.left(90)
+tu.forward(200)
+tu.left(90)
+tu.forward(200)
+tu.left(90)
