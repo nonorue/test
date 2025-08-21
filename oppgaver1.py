9ac3b696ec19f263529aee99a15cb6d196b0ab42
nonorue <nicolairue@outlook.com>
2025-08-21 21:36:43 +0200

    Add files via upload

--- oppgaver1.py
+++ oppgaver1.py
@@ -12,19 +12,98 @@ print("Etter fem år er nyet beløpet:", f'{beløp:.2f}',"kr")
 #%%
 #oppgave 3.2
 
-import numpy as n
-number = n.array(
+import numpy as np
+number = 8.02
+rundNed = np.floor(number)
+rundOpp = np.ceil(number)
+print(rundNed, rundOpp)
+#eller
+print(np.floor(number), np.ceil(number))
+#Her ser vi at floor og ceil funksjonene begge runder opp men da floor runder ned til nærmeste hele tall og ceil runder opp til nærmeste hele tall
 
 #%%
 #oppgave 3.3
+#svaret skal bli gitt med en Euklidsk formel for avstand, den er = kvadratrot av (xA-xB)**2+(yA-yB)**2
+A = (2.3, 8.1)
+xA = 2.3
+yA = 8.1
+B = (7.4, -13.5)
+xB = 7.4
+yB = -13.5
+
+import numpy as np
+
+avstand = np.sqrt(((xA-xB)**2)+((yA-yB)**2))
+#avstand = np.sqrt(((2.3-7.4)**2)+((8.1-(-13.5))**2))
+avstand = float(avstand)
+print("Avstand mellom A og B er:", f'{avstand:.3e}')
+
+
 #%%
 #oppgave 3.4
+
+xA = float(input("Kordinat for xA:"))
+yA = float(input("Kordinat for yA:"))
+
+xB = float(input("Kordinat for xB:"))
+yB = float(input("Kordinat for yB:"))
+
+import numpy as np
+
+avstand = np.sqrt(((xA-xB)**2)+((yA-yB)**2))
+#avstand = np.sqrt(((2.3-7.4)**2)+((8.1-(-13.5))**2))
+
+print("Avstand mellom A og B er:", f'{avstand:.3e}')
 #%%
 #oppgave 3.5
+#formel for overflate er = 4pir**2
+r = float(input("Skriv inn radius for kule:"))
+import math as m
+p = m.pi
+overflate = float(4*p*(r**2))
+#formel til volum er = V = 4pir**3/3
+volum = float(4*p*(r**3)/3)
+print("Overflate:", f'{overflate:.2f}', "Volum:", f'{volum:.2f}')
+
+
 #%%
 #oppgave 3.6
+
+litenKatet = float(input("Skriv inn lengde [m] på liten katet:"))
+hypot = float(input("Skriv inn lengde på [m] hypotenus:"))
+# k**2 + k**2 = h**2 så k**2 = h**2 - k**2
+# k = kvadratroden av k**2
+import numpy as nu
+storKatet = nu.sqrt((hypot**2) - (litenKatet**2))
+print("Lengde [m] på siste katet er:", f'{storKatet:.1f}')
+#omkrets =  h + k1 + k2 areal = g * h /2
+omkrets = hypot + litenKatet + storKatet
+areal = (litenKatet*storKatet)/2
+print("Trekanten har en omkrets [m] på:", f'{omkrets:.1f}', "og ett Areal [m^2] på:", f'{areal:.1f}')
 #%%
 #oppgave 3.7
+
+litenKatet = float(input("Skriv inn lengde [m] på liten katet:"))
+hypot = float(input("Skriv inn lengde på [m] hypotenus:"))
+# k**2 + k**2 = h**2 så k**2 = h**2 - k**2
+# k = kvadratroden av k**2
+import numpy as nu
+storKatet = nu.sqrt((hypot**2) - (litenKatet**2))
+print("Lengde [m] på siste katet er:", f'{storKatet:.1f}')
+#omkrets =  h + k1 + k2 areal = g * h /2
+omkrets = hypot + litenKatet + storKatet
+areal = (litenKatet*storKatet)/2
+print("Trekanten har en omkrets [m] på:", f'{omkrets:.1f}', "og ett Areal [m^2] på:", f'{areal:.1f}')
+#cos sin tan
+#%%
+import numpy as nu
+import math as ma
+a = 8
+b = 5
+c = 6.2
+abc = nu.array(8,5,6.2)
+sin = nu.degrees(abc)0
+print(sin)
 #%%
 #oppgave 3.8
 #%% 
